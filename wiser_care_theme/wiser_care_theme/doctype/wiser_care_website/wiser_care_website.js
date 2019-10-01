371472683f868f9658b875cecbbd9f50b76101d5
MostafaFekry <mostafa.fekry@gmail.com>
2019-10-01 06:21:21 +0000

    Update Files

--- wiser_care_theme/wiser_care_theme/doctype/wiser_care_website/wiser_care_website.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, Systematic and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Wiser Care Website', {
-	refresh: function(frm) {
-
-	}
-});
