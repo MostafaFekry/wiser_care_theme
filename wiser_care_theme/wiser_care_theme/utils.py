9892442803dffeeb5e02136c87c2eb4cc9144b60
MostafaFekry <mostafa.fekry@gmail.com>
2022-01-27 15:39:24 +0000

    Update for V13

--- wiser_care_theme/wiser_care_theme/utils.py
+++ wiser_care_theme/wiser_care_theme/utils.py
@@ -8,9 +8,21 @@ from frappe.utils import get_request_site_address, encode
 from frappe.model.document import Document
 from six.moves.urllib.parse import quote
 from frappe.website.router import resolve_route
-from erpnext.setup.doctype.item_group.item_group import get_group_item_count
 
+def get_group_item_count(item_group):
+	child_groups = ", ".join(['"' + i[0] + '"' for i in get_child_groups(item_group)])
+	return frappe.db.sql("""select count(*) from `tabItem`
+		where docstatus = 0 and show_in_website = 1
+		and (item_group in (%s)
+			or name in (select parent from `tabWebsite Item Group`
+				where item_group in (%s))) """ % (child_groups, child_groups))[0][0]
 
+def get_child_groups(item_group_name):
+	item_group = frappe.get_doc("Item Group", item_group_name)
+	return frappe.db.sql("""select name
+		from `tabItem Group` where lft>=%(lft)s and rgt<=%(rgt)s
+			and show_in_website = 1""", {"lft": item_group.lft, "rgt": item_group.rgt})
+			
 def update_website_context(context):
 	context["mostafa_test"] = frappe.form_dict._lang
 	website_languages_request = frappe.form_dict._lang
@@ -19,31 +31,31 @@ def update_website_context(context):
 	else:
 		context["website_languages_request"] = "en"
 		website_languages_request = "en"
-		
+
 	context.update(get_website_languages_details(website_languages_request))
 	context["website_languages_items"] = get_website_languages_items()
-	
+
 	website_settings = frappe.get_doc('Wiser Website Settings')
 	context["enable_multilanguage_support"] = website_settings.enable_multilanguage_support
 	context["display_language_picker_in_top_bar"] = website_settings.display_language_picker_in_top_bar
-	
+
 	context["facebook_link"] = website_settings.facebook_link
 	context["linked_in_link"] = website_settings.linked_in_link
 	context["twitter_link"] = website_settings.twitter_link
 	context["instagram_link"] = website_settings.instagram_link
 	context["google_link"] = website_settings.google_link
-	
+
 	# Mostafa return item groups sidebar items
 	context["sidebarshow_item_group"] = sidebarshow_item_group()
-	
+
 	# Mostafa return medication type selected items
 	context["medication_type"] = get_medication_type()
-	
-	
+
+
 def get_website_languages_items():
 	all_language_items = frappe.db.sql("""\
 		select * from `tabWebsite Languages`
-		where parent='Wiser Website Settings' 
+		where parent='Wiser Website Settings'
 		order by idx asc""", as_dict=1)
 
 	website_languages_items = [d for d in all_language_items]
@@ -79,13 +91,13 @@ def sidebarshow_item_group():
 		select IG.item_group_name,IG.show_in_website,IG.route,IG.is_group,IG.parent_item_group,IG.lft,IG.rgt from `tabItem Group` IG
 		where IG.show_in_website=1 and IG.parent_item_group = 'Medicinal Effect'
 		order by IG.lft asc""", as_dict=1)
-		
+
 	data = [d for d in item_group]
-	
+
 	sidebarshow_item_group = adjust_group_item_count_items(data)
-	
+
 	return sidebarshow_item_group
-	
+
 def adjust_group_item_count_items(data):
 	adjusted_data = []
 
@@ -98,7 +110,7 @@ def adjust_group_item_count_items(data):
 
 	return adjusted_data
 
-# Mostafa return medication type selected items	
+# Mostafa return medication type selected items
 def get_medication_type(product_group=None):
 	if product_group:
 		item_group = frappe.get_cached_doc('Item Group', product_group)
@@ -106,12 +118,12 @@ def get_medication_type(product_group=None):
 			# return child item groups if the type is of "Is Group"
 			adjusted_data = []
 			return adjusted_data
-	
+
 	#medication_type = frappe.db.sql("""\select MT.medication_type, MT.name, MT.image from `tabMedication Type` MT inner join `tabItem` I on MT.name = I.medication_type	and I.show_in_website = 1 and I.docstatus = 0 and (I.item_group = '{item_group_d}' or I.name in (select parent from `tabWebsite Item Group` where item_group = '{item_group_d}')) where MT.show_in_website =1 group by MT.medication_type order by MT.idx asc""".format(item_group_d=product_group), as_dict=1)
 
 	medication_type = frappe.db.sql("""\
 	select MT.medication_type, MT.name, MT.image from `tabMedication Type` MT inner join `tabItem` I on MT.name = I.medication_type	and I.show_in_website = 1 and I.docstatus = 0 where MT.show_in_website =1 group by MT.medication_type order by MT.idx asc""", as_dict=1)
-	
+
 	medication_type_item = [d for d in medication_type]
-	
+
 	return medication_type_item
